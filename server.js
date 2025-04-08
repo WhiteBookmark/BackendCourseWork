d6799c912f53a2305a9cb157dccb2d2f8cbdde2e
White Bookmark <space.saverofcomputer@gmail.com>
2025-04-08 15:13:08 +0400

    add: Images

--- server.js
+++ server.js
@@ -93,7 +93,7 @@ async function run() {
 			}
 		});
 
-		// Update any attribute of the lesson collection
+		// PUT /lessons Update any attribute of the lesson collection
 		app.put('/lessons', async (req, res) => {
 			try {
 				const lesson = req.body;
